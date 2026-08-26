e47ba4945a7b49a9aaaa7bc44d2c884db639d9db
Rowan McDonald <22102097+RowanMcDonald@users.noreply.github.com>
2026-08-26 12:11:37 -0400

    Scaffold audd using bundle gem

---
+++ spec/audd_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Audd do
+  it "has a version number" do
+    expect(Audd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
