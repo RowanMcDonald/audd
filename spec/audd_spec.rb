90860a018533b822e29d2fb690d9ac84d9ef8716
Rowan McDonald <22102097+RowanMcDonald@users.noreply.github.com>
2026-08-26 19:29:01 -0400

    Stub out error spec

--- spec/audd_spec.rb
+++ spec/audd_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe Audd do
     expect(Audd::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(Audd.new("test").).to eq(true)
+  xit "raises error when the api errors" do
+    # TODO: setup error mock
+    #
+    expect { 
+      Audd.new("test").recognize_enterprise(file: file).execute! 
+    }.to raise_error Audd::Error
   end
 end
